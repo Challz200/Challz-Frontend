fe697c39852da2af3b6d04261f276d87b920ddcb
Challz200 <challzspprt@gmail.com>
2025-08-01 03:38:30 -0400

    Update layout.tsx
    Fix Sintaxis

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type React from "react"
 import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-<<<<<<< HEAD
-import { AuthBackendProvider } from "@/contexts/auth-backend-context"
-=======
 import { AuthProvider } from "@/contexts/auth-context"
->>>>>>> a2352e7dac0ebfeb1e0d079c1703d9a5b8a10d44
 
 export const metadata = {
   title: "Challz - Desafía tu rutina. Reta tu mundo.",
   description: "Una aplicación de retos diarios para inspirar creatividad, conexión social y crecimiento personal.",
-    generator: 'v0.dev'
+  generator: 'Chito'
 }
 
 export default function RootLayout({
@@ -22,11 +18,7 @@ export default function RootLayout({
     <html lang="es" suppressHydrationWarning>
       <body className="min-h-screen bg-black font-sans antialiased">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-<<<<<<< HEAD
-          <AuthBackendProvider>{children}</AuthBackendProvider>
-=======
           <AuthProvider>{children}</AuthProvider>
->>>>>>> a2352e7dac0ebfeb1e0d079c1703d9a5b8a10d44
         </ThemeProvider>
       </body>
     </html>
